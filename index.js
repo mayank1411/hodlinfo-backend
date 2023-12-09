c0f2d2728d37320fc580541b5f08a166385a87bd
Mayank <mayank030mink@gmail.com>
2023-12-09 12:28:41 +0530

    changes

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors')
 const {connectDB} = require('./db')
 const Stocks = require('./model/Stock')
 const bodyParser = require('body-parser')
 const PORT = 5000 || process.env.PORT
 ;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({
       extended: false
    }));
